5f9a1176ea3ba2527f1af071f3aff631dde21c5f
Mikael Brudfors <brudfors@gmail.com>
2018-07-19 22:38:37 +0100

    Added colorbar

--- code/verbose/show_img.m
+++ code/verbose/show_img.m
@@ -16,31 +16,31 @@ for c=1:C % Loop over channels
     subplot(C,2,2*(c - 1) + 1)
     if strcmpi(modality,'CT')
         dm = size(X{c});
-        imagesc(X{c}(:,:,floor(dm(3)/2) + 1)',[0 100]);
+        imagesc(X{c}(:,:,floor(dm(3)/2) + 1)',[0 100]); colorbar;
     else
         if iscell(X{c})
             dm = size(X{c}{1});
-            imagesc(X{c}{1}(:,:,floor(dm(3)/2) + 1)'); % Show only first within-channel image
+            imagesc(X{c}{1}(:,:,floor(dm(3)/2) + 1)'); colorbar; % Show only first within-channel image
         else
             dm = size(X{c});
-            imagesc(X{c}(:,:,floor(dm(3)/2) + 1)');
+            imagesc(X{c}(:,:,floor(dm(3)/2) + 1)'); colorbar;
         end
     end
     axis off image; colormap(gray);       
     if iscell(X{c})
-        title(['sd=' num2str(round(sd{c}(1),2))])
+        title(['sd=' num2str(round(sd{c}(1),3))])
     else
-        title(['sd=' num2str(round(sd(c),2))])
+        title(['sd=' num2str(round(sd(c),3))])
     end
 
     subplot(C,2,2*(c - 1) + 2)
     if strcmpi(modality,'CT')
-        imagesc(Y{c}(:,:,floor(dm1(3)/2) + 1)',[0 100]); 
+        imagesc(Y{c}(:,:,floor(dm1(3)/2) + 1)',[0 100]); colorbar; 
     else
-        imagesc(Y{c}(:,:,floor(dm1(3)/2) + 1)'); 
+        imagesc(Y{c}(:,:,floor(dm1(3)/2) + 1)'); colorbar; 
     end
     axis off image; colormap(gray);    
-    title(['lambda=' num2str(round(lambda(c),2))])    
+    title(['lambda=' num2str(round(lambda(c),3))])    
 end
 drawnow
 %==========================================================================  
